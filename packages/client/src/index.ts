d235a2c8b565680f7b8605735037656373b908ed
oconnor-ryan <oconnor.ryan.m@proton.me>
2024-01-18 09:30:48 -0600

    Changed folder structure of client to separate NodeJS code from client-side Javacript.

--- packages/client/src/index.ts
+++ packages/client/src/index.ts
@@ -1,3 +1,7 @@
+//This is the only NodeJS script, which contains helper functions in order
+//to help serve the static HTML/CSS/Javascript in the src/webroot folder
+
+//get the root folder where all website assets are stored
 export function getStaticFileRoot() {
-  return import.meta.resolve("../");
+  return import.meta.resolve("./webroot");
 }
